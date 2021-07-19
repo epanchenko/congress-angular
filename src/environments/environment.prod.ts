88b085f3868892c7f977e77dd9d52bd38ee97757
Eric Panchenko <epanchenko@gmail.com>
2021-07-19 02:57:12 -0400

    2.04

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiURL: '//http://congressapispring.us-east-2.elasticbeanstalk.com/api/v2/',
+  apiURL: 'http://congressapispring.us-east-2.elasticbeanstalk.com/api/v2/',
 };
 
 export const nosqlEnvironment = {
   production: true,
-  apiURL: '//congressapinodejs.us-east-2.elasticbeanstalk.com/api/v1/',
+  apiURL: 'http://congressapinodejs.us-east-2.elasticbeanstalk.com/api/v1/',
 };
